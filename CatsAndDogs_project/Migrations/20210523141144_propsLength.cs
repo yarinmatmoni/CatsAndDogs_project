dc7b1f4cbda0c49a544a56ac551af116d36c45ce
yarinmatmoni <74259063+yarinmatmoni@users.noreply.github.com>
2021-05-23 17:27:34 +0300

    update validation for cats and dogs

---
+++ CatsAndDogs_project/Migrations/20210523141144_propsLength.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CatsAndDogs_project.Migrations
+{
+    public partial class propsLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Dogs",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Dogs",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Cats",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Cats",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Dogs",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Dogs",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Cats",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Cats",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+        }
+    }
+}
--- CatsAndDogs_project/Models/Cats.cs
+++ CatsAndDogs_project/Models/Cats.cs
@@ -10,35 +10,37 @@ namespace CatsAndDogs_project.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "זהו שדה חובה")]
         [Display(Name = "שם")]
+        [StringLength(50, MinimumLength = 3 , ErrorMessage = "אורך השם חייב להחיל בין 3-50 תווים")]
         public String Name { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "זהו שדה חובה")]
         [Display(Name = "גזע")]
         public String Type { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "זהו שדה חובה")]
         [Display(Name = "גיל")]
         public int Age { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "זהו שדה חובה")]
         [Display(Name = "מין")]
         public String Gender { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "זהו שדה חובה")]
         [Display(Name = "צבע")]
         public String Color { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "זהו שדה חובה")]
         [Display(Name = "תוחלת חיים")]
         public String LifeExpectancy { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "זהו שדה חובה")]
         [Display(Name = "תיאור")]
+        [StringLength(200, MinimumLength = 10 , ErrorMessage = "התיאור חייב להחיל בין 20-200 תווים")]
         public String Description { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "זהו שדה חובה")]
         [Display(Name = "צרף קישור לתמונה")]
         public String Image { get; set; }
     }

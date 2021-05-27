161bcfe2cce0be6a5f4d9721453e57fa9dc62567
yarinmatmoni <74259063+yarinmatmoni@users.noreply.github.com>
2021-05-27 11:22:12 +0300

    Share DB

---
+++ CatsAndDogs_project/Migrations/20210527082001_ShareDB.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CatsAndDogs_project.Migrations
+{
+    public partial class ShareDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

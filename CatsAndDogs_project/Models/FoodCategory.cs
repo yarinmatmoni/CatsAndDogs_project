1af272eb9af880bf08f7665c320768c4bfc5dcd1
LilachBachar <Lilachbac@gmail.com>
2021-08-16 15:15:53 +0300

    create food and foodcategory models

---
+++ CatsAndDogs_project/Models/FoodCategory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatsAndDogs_project.Models
+{
+    public class FoodCategory
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "שם המוצר:")]
+        [Required(ErrorMessage = "זהו שדה חובה")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "אורך השם חייב להחיל בין 3-50 תווים")]
+        public string Name { get; set; }
+
+
+        public List<Food> FoodList { get; set; }
+
+
+    }
+}

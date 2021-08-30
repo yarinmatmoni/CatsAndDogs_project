a2447c34226a37a86afcb9b6f3a93a1f88d31788
EdenReuven <74300459+EdenReuven@users.noreply.github.com>
2021-08-31 00:12:21 +0300

    fix create guide add style for index guide

---
+++ CatsAndDogs_project/Migrations/20210830205220_fix.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CatsAndDogs_project.Migrations
+{
+    public partial class fix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AvgWeight",
+                table: "GuideDog",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(5)",
+                oldMaxLength: 5);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AvgLife",
+                table: "GuideDog",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(5)",
+                oldMaxLength: 5);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AvgHeight",
+                table: "GuideDog",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(7)",
+                oldMaxLength: 7);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AvgWeight",
+                table: "GuideDog",
+                type: "nvarchar(5)",
+                maxLength: 5,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AvgLife",
+                table: "GuideDog",
+                type: "nvarchar(5)",
+                maxLength: 5,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AvgHeight",
+                table: "GuideDog",
+                type: "nvarchar(7)",
+                maxLength: 7,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldMaxLength: 12);
+        }
+    }
+}
